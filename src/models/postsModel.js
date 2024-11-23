4340cf86b74c0d8ff1e42a334b60434c374691bc
Jefferson Xavier <67285300+parajotaxis@users.noreply.github.com>
2024-11-22 21:56:08 -0300

    first commit

---
+++ src/models/postsModel.js
@@ -0,0 +1,15 @@
+import conectarAoBanco from "../config/dbconfig.js";
+
+const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
+
+export async function getTodosPosts() {
+    const db = conexao.db("imersao-instabytes");
+    const colecao = db.collection("posts");
+    return colecao.find().toArray();
+}
+
+export async function criarPost(novoPost) {
+    const db = conexao.db("imersao-instabytes");
+    const colecao = db.collection("posts");
+    return colecao.insertOne(novoPost);
+}
