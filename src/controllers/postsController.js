e08b0e9e586b532d3789cc82cc545fd83f7cc0e0
Jefferson Xavier <parajotaxis@gmail.com>
2024-11-23 20:31:53 -0300

    fim da aula 4

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,4 +1,5 @@
 import {getTodosPosts , criarPost} from "../models/postsModel.js";
+import fs from "fs";
 
 export async function listarPosts(req, res) {
     const posts = await getTodosPosts();
@@ -17,4 +18,22 @@ export async function postarNovoPost(req, res) {
     }
     
     
+}
+
+export async function uploadImagem(req, res) {
+    const novoPost = {
+        descricao: "",
+        imgUrl: req.file.originalname,
+        alt: ""
+    };
+
+    try {
+        const postCriado = await criarPost(novoPost);
+        const imagemAtualizada = `uploads/${postCriado.insertedId}.png`
+        fs.renameSync(req.file.path, imagemAtualizada);
+        res.status(200).json(postCriado);  
+    } catch(erro) {
+        console.error(erro.message);
+        res.status(500).json({"Erro":"Falha na requisição"})
+    }
 }
--- src/routes/postsRoute.js
+++ src/routes/postsRoute.js
@@ -1,12 +1,44 @@
+// Importa o módulo Express para criar a aplicação web
 import express from "express";
-import { listarPosts, postarNovoPost } from "../controllers/postsController.js";
 
+// Importa o módulo Multer para lidar com o upload de arquivos
+import multer from "multer";
+
+// Importa funções do controlador de posts para lidar com as rotas
+import { listarPosts, postarNovoPost, uploadImagem } from "../controllers/postsController.js";
+
+// Configura o armazenamento de arquivos, definindo o diretório de destino
+const storage = multer.diskStorage({
+  // Define a função para determinar o destino do arquivo
+  destination: function (req, file, cb) {
+    // Define o diretório de uploads
+    cb(null, 'uploads/'); 
+  },
+  // Define a função para determinar o nome do arquivo
+  filename: function (req, file, cb) {
+    // Utiliza o nome original do arquivo
+    cb(null, file.originalname); 
+  }
+});
+
+// Cria uma instância do middleware Multer com as configurações definidas
+const upload = multer({ dest: "./uploads" , storage });
+
+// Define a função que configura as rotas da aplicação
 const routes = (app) => {
-    app.use(express.json());
+  // Habilita o parsing de JSON no corpo das requisições
+  app.use(express.json());
 
-    app.get("/posts", listarPosts);
-    app.post("/posts", postarNovoPost);
+  // Define a rota para listar todos os posts
+  app.get("/posts", listarPosts);
+
+  // Define a rota para criar um novo post
+  app.post("/posts", postarNovoPost);
+
+  // Define a rota para fazer upload de uma imagem
+  app.post("/upload", upload.single("imagem"), uploadImagem);
 }
 
+// Exporta a função de rotas para ser utilizada em outros módulos
 export default routes;
 
